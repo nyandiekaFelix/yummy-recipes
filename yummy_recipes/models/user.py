28fdc1b7d631bc37896238a9544aef728f72c1a5
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-10-17 22:26:14 +0300

    [feat]: create user and recipe models

--- yummy_recipes/models/user.py
+++ yummy_recipes/models/user.py
@@ -1,14 +1,14 @@
-''' User model '''
+import random
+
+USERS = []
 
 class User(object):
-    
+    ''' User model '''
     def __init__(self, email, first_name, second_name, password):
         ''' Initialize user '''
+        self.user_id = random.randint(1, 1000)
         self.email = email
         self.first_name = first_name
         self.second_name = second_name
         self.password = password
-        
-    
-
-    
+           
