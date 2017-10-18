49ebdb7d2c3c05329d447ab60f34cf649204d39c
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-10-18 14:20:27 +0300

    [feat]: use csrf protection

--- yummy_recipes/__init__.py
+++ yummy_recipes/__init__.py
@@ -1,6 +1,8 @@
-import os
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
-app = Flask(__name__)
+APP = Flask(__name__)
+APP.secret_key = 'very_secret' # to be set as env variale later
+csrf = CSRFProtect(APP)
 
-from yummy_recipes.models import user
+from yummy_recipes import views
