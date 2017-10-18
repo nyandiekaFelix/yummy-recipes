b6b5157c606fe70f347b1b6340fd03a07edc67af
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-10-18 21:12:16 +0300

    [feat]: create custom 404 page

--- yummy_recipes/views.py
+++ yummy_recipes/views.py
@@ -6,13 +6,13 @@ from yummy_recipes.models.user import User, USERS
 from yummy_recipes.models.recipe import RecipeCategory, Recipe, CATEGORIES  
 
 @APP.route('/')
+@APP.route('/home')
 def home():
-    pass
+    return render_template('home.html')
 
-@APP.route('/myrecipes')
-def my_recipes():
-    pass
-
+@APP.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html')
 @APP.route('/signup', methods=['POST', 'GET'])
 def signup():
     ''' Create New user '''
@@ -29,13 +29,13 @@ def signup():
                 else:
                     user = User(email, first_name, last_name, password)
                     USERS.append(user)
-                    return redirect(url_for('myrecipes'))
+                    return redirect(url_for('home'))
         else:
             user = User(email, first_name, last_name, password)
             USERS.append(user)
-            return redirect(url_for('myrecipes'))
-        
-        return render_template("signin.html", form=form)
+            return redirect(url_for('home'))
+    elif request.method == 'GET':
+        return render_template("signup.html", form=form)
 
 @APP.route('/login', methods=['POST', 'GET'])
 def login():
@@ -50,9 +50,20 @@ def login():
                 if user.password == password:
                     session["logged_in"] = True
                     session["email"] = user.email
-                    return redirect(url_for('myrecipes'))
+                    return redirect(url_for('home'))
                 else:
                     flash('Wrong password!')
+                    return redirect(url_for('home'))
             else:
                 flash('User not found!')
-    return render_template("login.html", form=form)
+                return redirect(url_for('home'))
+    elif request.method == 'GET':
+        return render_template("login.html", form=form)
+
+@APP.route('/myrecipes')
+def my_recipes():
+    return render_template('recipes.html')
+
+@APP.route('/mycategories')
+def my_categories():
+    return render_template('recipe_category.html')
