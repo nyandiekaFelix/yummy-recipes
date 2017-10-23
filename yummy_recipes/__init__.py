ab1fe9c5583fc916c6d82fea5f9a04ae74da6ca6
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-10-23 16:23:32 +0300

    [fix]: correct typo

--- yummy_recipes/__init__.py
+++ yummy_recipes/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_wtf.csrf import CSRFProtect
 
 APP = Flask(__name__)
-APP.secret_key = 'very_secret' # to be set as env variale later
+APP.secret_key = 'very_secret' # to be set as env variable later
 csrf = CSRFProtect(APP)
 
 from yummy_recipes import views
