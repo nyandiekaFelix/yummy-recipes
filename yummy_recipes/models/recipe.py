35544aa362bda3f274cbc9b349cdcb97946d4238
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-10-26 06:22:31 +0300

    [feat]: generate unique ID

--- yummy_recipes/models/recipe.py
+++ yummy_recipes/models/recipe.py
@@ -1,18 +1,22 @@
+from uuid import uuid4
+
 ''' Models for Recipe Category and individual category '''
 
 class RecipeCategory(object):
     ''' Recipe Category Model '''
     def __init__(self, category_name):
+        self.category_id = str(uuid4())
         self.category_name = category_name
+        self.recipes = {}
         
     def __repr__(self):
         return '<%(category_name)s obj>' % dict(category_name=self.category_name)
 
 class Recipe(object):
     ''' Recipe Model '''
-    def __init__(self, category_name, recipe_name, ingredients, 
+    def __init__(self, recipe_name, ingredients, 
                  instructions):
-        self.category_name = category_name
+        self.recipe_id = str(uuid4())
         self.recipe_name = recipe_name
         self.ingredients = ingredients
         self.instructions = instructions
