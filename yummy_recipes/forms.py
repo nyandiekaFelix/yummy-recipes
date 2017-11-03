6250e5ed84bce81cbf45f30c297e7aa08fe987ff
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-11-03 04:08:35 +0300

    [feat]: exclude numbers from category or recipe names

--- yummy_recipes/forms.py
+++ yummy_recipes/forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import Form
 from wtforms import StringField, PasswordField, SelectField
-from wtforms.validators import Email, InputRequired, DataRequired, Regexp, EqualTo
+from wtforms.validators import Email, InputRequired, DataRequired, Regexp, EqualTo, Length
 
 class Signup(Form):
     ''' User registration form '''
     email = StringField('Email:', validators=[Email(), InputRequired()])
-    password = PasswordField('Password:', validators=[InputRequired(), 
+    password = PasswordField('Password:', validators=[InputRequired(), Length(min=4, max=25),
                 EqualTo('password_confirm', 
                 message=(u'Passwords Do Not Match')),
                 Regexp("^(?=.*?[A-Z]).*[0-9]",  0,
-                        'Should contain atleast one digit and one uppercase letter')])
+                        'Should contain letters and at least one digit and one uppercase letter')])
 
     password_confirm = PasswordField('Confirm password:')
     
@@ -23,23 +23,23 @@ class Category(Form):
     category_name = StringField(
         'New Category:', validators=[InputRequired(),
                                         DataRequired(),
-                                        Regexp("^[A-Za-z0-9_-]+( +[A-Za-z0-9_-]+)*$",
+                                        Regexp("^[A-Za-z]+( +[A-Za-z]+)*$",
                                                 0,
-                                                'Use only letters numbers and spaces')])
+                                                'Use only letters and spaces')])
 
 class RecipeForm(Form):
     ''' individual recipes form '''
     recipe_name = StringField(
         'Recipe Name: ', validators=[InputRequired(),
                                         DataRequired(),
-                                        Regexp("^[A-Za-z0-9_-]+( +[A-Za-z0-9_-]+)*$",
+                                        Regexp("^[A-Za-z]+( +[A-Za-z]+)*$",
                                                 0,
-                                                'Use only letters numbers and spaces')])
+                                                'Use only letters and spaces')])
     ingredients = StringField(
-        'Ingredients: ', validators=[InputRequired(),
+        'Ingredients: ', validators=[InputRequired(), Length(min=5, max=200),
                                         DataRequired()])
     
     instructions = StringField(
-        'Instructions: ', validators=[InputRequired(),
+        'Instructions: ', validators=[InputRequired(), Length(min=5, max=200),
                                         DataRequired()])
     
