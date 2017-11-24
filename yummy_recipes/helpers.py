1dedc001138ed8d383d1e3318b587f84cd51fe76
nyandiekaFelix <nyandiekafelix@gmail.com>
2017-11-24 10:55:41 +0300

    [chore]: delete lower_email function

--- yummy_recipes/helpers.py
+++ yummy_recipes/helpers.py
@@ -1,16 +1,5 @@
 ''' This file contains utility functions '''
 
-
-def lower_email(email):
-    ''' This method converts an email into lowercase before storage so
-    as to avoid duplicates based on the string's case.
-    '''
-
-    email_name, domain_part = email.strip().rsplit('@', 1)
-    email = '@'.join([email_name.lower(), domain_part.lower()])
-    return email
-
-
 def check_for_duplicate(obj, value_string):
     ''' This method checks if a value exists in an object '''
     exists = False
